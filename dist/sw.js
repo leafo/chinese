108530fa2a1467b354b56abddb187753150f29cf
leaf corcoran <leafot@gmail.com>
2026-04-07 13:55:59 -0700

    add service worker

---
+++ dist/sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = "chinese-v1";
+
+const PRECACHE_URLS = [
+  "./",
+  "./index.html",
+  "./index.js",
+  "./index.css",
+  "./manifest.json",
+  "./icon.svg",
+  "./icon-192.png",
+  "./icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_URLS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match("./index.html"))
+    );
+    return;
+  }
+
+  // Stale-while-revalidate: serve from cache immediately, update in background
+  event.respondWith(
+    caches.open(CACHE_NAME).then((cache) =>
+      cache.match(event.request).then((cached) => {
+        const fetched = fetch(event.request).then((response) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+        return cached || fetched;
+      })
+    )
+  );
+});
